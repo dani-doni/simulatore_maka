b16ba82452755c8c83cd0e04160bca817910f788
dani-doni <128325494+dani-doni@users.noreply.github.com>
2024-05-14 16:10:09 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,24 +1,11 @@
 import streamlit as st
-import pandas as pd 
-import gspread
-from google.oauth2 import service_account
+from streamlit_gsheets import GSheetsConnection
 
 # Create a connection object.
-credentials = service_account.Credentials.from_service_account_info(
-    st.secrets["gcp_service_account"],
-    scopes=[
-        "https://www.googleapis.com/auth/spreadsheets","https://www.googleapis.com/auth/drive"
-    ],
-)
-conn = connect(credentials=credentials)
-client=gspread.authorize(credentials)
+conn = st.connection("gsheets", type=GSheetsConnection)
 
-sheet_id = 'results'
-csv_url = f"https://docs.google.com/spreadsheets/d/{sheet_id}/export?format=csv"
-database_df = pd.read_csv(csv_url, on_bad_lines='skip')
+df = conn.read()
 
-database_df = database_df.astype(str)
-sheet_url = st.secrets["private_gsheets_url"] #this information should be included in streamlit secret
-sheet = client.open_by_url(sheet_url).sheet1
-sheet.update([database_df.columns.values.tolist()] + database_df.values.tolist())
-st.success('Data has been written to Google Sheets')
+# Print results.
+for row in df.itertuples():
+    st.write(f"{row.name} has a :{row.pet}:")
