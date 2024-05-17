1b25cb9ec04e98eb15d741f83e504fcc786c25c8
dani-doni <128325494+dani-doni@users.noreply.github.com>
2024-05-17 11:25:10 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,8 @@
 import streamlit as st
 import json
-
+import firebase_admin
+from firebase_admin import credentials
 key_dict = json.loads(st.secrets["textkey"])
-key_dict
-creds = credentials.Certificate(key_dict)
-db = firestore.Client(credentials=creds, project="maka")
-
-performance = db.collection("performance")
-
-for p in performance.stream():
-	st.write("The start is: ", p.start)
-	st.write("The finish is: ", p.finish)
-
+cred = credentials.Certificate(key_dict)
+firebase_admin.initialize_app(cred)
 
