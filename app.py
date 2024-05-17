76ffd69dc5a65afea45f72584f6df791405f1f3f
dani-doni <128325494+dani-doni@users.noreply.github.com>
2024-05-17 11:41:52 +0200

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,10 @@ import streamlit as st
 import json
 import firebase_admin
 from firebase_admin import credentials
+from google-cloud-firestore import firestore
 key_dict = json.loads(st.secrets["textkey"])
 cred = credentials.Certificate(key_dict)
-firebase_admin.initialize_app(cred)
+db = firestore.Client.from_service_account_json(key_dict)
+
+
 
