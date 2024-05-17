da72ce91270e0291b644fa60606d07a091977a29
dani-doni <128325494+dani-doni@users.noreply.github.com>
2024-05-17 11:14:20 +0200

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@
 import streamlit as st
 import json
 key_dict = json.loads(st.secrets["textkey"])
+key_dict
 creds = service_account.Credentials.from_service_account_info(key_dict)
 db = firestore.Client(credentials=creds, project="maka")
 
