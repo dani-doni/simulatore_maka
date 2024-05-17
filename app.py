ec5327877041b0c43a42c359bef090c8de15aa34
dani-doni <128325494+dani-doni@users.noreply.github.com>
2024-05-17 11:24:11 +0200

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import json
 
 key_dict = json.loads(st.secrets["textkey"])
 key_dict
-creds = service_account.Credentials.from_service_account_info(key_dict)
+creds = credentials.Certificate(key_dict)
 db = firestore.Client(credentials=creds, project="maka")
 
 performance = db.collection("performance")
@@ -11,3 +11,5 @@ performance = db.collection("performance")
 for p in performance.stream():
 	st.write("The start is: ", p.start)
 	st.write("The finish is: ", p.finish)
+
+
